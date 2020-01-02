b4b8c0f2f2b238f3f26c9d3dd688187df7881b92
jk-kevin <kevinvuongcs@gmail.com>
2020-01-01 19:07:10 -0500

    finish initial layout

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 /*
-  Kevin Vuong
-  June 16, 2019
-  Personal Website
+Kevin Vuong
+December 31, 2019
+What-to-Eat
 */
-
 
 //Load HTTP module
 const express = require('express')
@@ -14,18 +13,11 @@ const port = 3000;
 
 //Start the server using express
 app.get('/', (req, res) => {
-  //res.send('Hello World!')
-
-//app.use(express.static(__dirname + '/html/styles'))
-
-
-res.sendFile(__dirname + '/html/index.html') // send html file to client
-app.use(express.static(path.join(__dirname, 'public'))); //send static files like .css files to client
+  res.sendFile(__dirname + '/html/index.html') // send html file to client
+  app.use(express.static(path.join(__dirname, 'public'))); //send static files like .css files to client
 });
 
 //provide static server
-
-
 app.listen(port, () => {
   console.log('Listening on port 3000!')
   console.log('Test at http://127.0.0.1:3000/')
