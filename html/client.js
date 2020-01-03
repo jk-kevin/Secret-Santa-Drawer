42552131b9dcd953576ba47fa2c381e6e4ae5b01
jk-kevin <kevinvuongcs@gmail.com>
2020-01-03 11:47:18 -0500

    Randomize food choices

---
+++ html/client.js
@@ -0,0 +1,13 @@
+/*
+Kevin Vuong
+December 31, 2019
+What-to-Eat
+*/
+
+let foods = ["Pizza", "Burgers", "Chinese", "Thai", "Mexican", "Pho", "Shawarma", "Breakfast", "Korean", "Italian", "Greek", "Indian", "Poutine", "Fried Chicken", "Sandwiches", "Sushi"];
+
+function randomizeFood() {
+    let randomInt =  Math.floor(Math.random() * foods.length);
+    console.log(randomInt);
+    document.getElementById("food").innerHTML = foods[randomInt];
+}
--- server.js
+++ server.js
@@ -11,14 +11,17 @@ const app = express();
 const hostname = '127.0.0.1';
 const port = 3000;
 
-//Start the server using express
+//provide static server
+//app.use(express.static(__dirname + '/html'));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/html/index.html') // send html file to client
+  //res.sendFile('/html/index.html', {root: __dirname})
+  app.use(express.static(path.join(__dirname, 'html'))); //send static files like .js files to client
   app.use(express.static(path.join(__dirname, 'public'))); //send static files like .css files to client
 });
-
-//provide static server
+//Start the server using express
 app.listen(port, () => {
-  console.log('Listening on port 3000!')
-  console.log('Test at http://127.0.0.1:3000/')
+  console.log('Listening on port 3000!');
+  console.log('Test at http://127.0.0.1:3000/');
 });
