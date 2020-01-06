2c86bfa0ecdcedd97a34434c9eb2f942fabe207e
jk-kevin <kevinvuongcs@gmail.com>
2020-01-06 15:25:38 -0500

    finish emailing functionality

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const path = require('path')
 const bodyParser = require('body-parser'); // for http post messages
 const nodemailer = require('nodemailer');
 const app = express();
-const hostname = '127.0.0.1';
 const port = 3000;
 
 //provide static server
@@ -22,26 +21,21 @@ app.get('/', (req, res) => {
   app.use(express.static(path.join(__dirname, 'html'))); //send static files like .js files to client
   app.use(express.static(path.join(__dirname, 'public'))); //send static files like .css files to client
 });
+
 app.post('/send-email', (req, res) => {
   let data = req.body;
-  console.log(data);
-  console.log(Object.keys(data).length); // num of keys in data
-  let numValues = Object.keys(data).length - 1;
   let participants = new Map();
   let values = Object.values(data); // parse data from object to array
+  let numValues = values[0].length;
 
   // pair names with emails
-  for (let i = 0; i < numValues-1; i+=2) {
-    participants.set(values[i], values[i+1]);
+  for (let i = 0; i < numValues; i++) {
+    participants.set(values[0][i], values[1][i]);
   }
   console.log("paired map");
   console.log(participants);
-  
   // draw names then send emails
-  drawNames(participants);
-  //sendEmails(participants, drawNames(participants));
-
-
+  sendEmails(participants, drawNames(participants));
   res.end()
 });
 
@@ -51,20 +45,18 @@ app.listen(port, () => {
   console.log('Test at http://127.0.0.1:3000/');
 });
 
-function drawNames(names) { // we probably don't need a callback here
+function drawNames(names) {
   let result = new Map();
-  let givers = Array.from(names.keys())[0]; // dunno why I get an array nested in another block
+  let givers = Array.from(names.keys());
   let receivers = [...givers]; //spread operator to duplicate array instead of refrencing it
   let numPlayers = givers.length;
 
-  //console.log("givers = " + givers + typeof givers);
-  //console.log(givers);
-  //console.log(receivers);
-  //console.log("numPlayers = " + numPlayers); // THE NUMBER OF PLAYERS IS WRONG!!!
+  console.log("Drawing names!");  // it doesn't work when there is no console.log in this function?
   for (let i = 0; i < numPlayers; i++) {
     while (true) {
       let randomInt = Math.floor(Math.random() * numPlayers);
 
+      //console.log(i);
       if (randomInt !== i && receivers[randomInt] !== null) {
         result.set(givers[i], receivers[randomInt]);
         receivers[randomInt] = null;
@@ -77,7 +69,9 @@ function drawNames(names) { // we probably don't need a callback here
   return result;
 }
 
-function sendEmails(names, pairings) {
+function sendEmails(namesEmails, pairings) { //using nodemailer
+  let players = Array.from(namesEmails.keys());
+  let numPlayers = players.length;
   var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -86,18 +80,20 @@ function sendEmails(names, pairings) {
     }
   });
 
-  var mailOptions = {
-    from: 'KevinSecretSantaBot@gmail.com',
-    to: 'secretsantamailtest@yahoo.com',
-    subject: 'Sending Email using Node.js',
-    text: 'That was easy!'
-  };
-
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
+  for (let i = 0; i < numPlayers; i++) {
+    let name = players[i];
+    var mailOptions = {
+      from: 'KevinSecretSantaBot@gmail.com',
+      to: namesEmails.get(name),
+      subject: 'Secret Santa Draw',
+      text: 'You are giving to ' + pairings.get(name) 
+    };
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+  }
 }
